17415bfbc9e9e23e2a5042815d7ed99fa97aa3e4
7BxDWoKVT4 <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-22 13:19:54 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import time
+import json
+import gzip
 from seleniumwire.undetected_chromedriver import Chrome
 
 def request_interceptor(request):
@@ -13,7 +15,10 @@ def request_interceptor(request):
 		request.headers['Content-Length'] = str(len(request.body))
 
 def response_interceptor(request, response):
-	print(request)
+	if "https://hcaptcha.com/getcaptcha" in request.url:
+		body = gzip.decompress(response.body).decode('utf-8')
+		data = json.loads(body)
+		print(data['generated_pass_UUID'])
 
 driver = Chrome(executable_path="./chromedriver")
 driver.request_interceptor = request_interceptor
